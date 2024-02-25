7a54be8ad461059f8be7e18b962ac69669ca8a0c
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2024-02-26 01:23:37 +0900

    各TLの保持件数を指定

---
+++ src/util/ArrayLengthControl.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+const MAX_LENGTH = 500
+
+export function ArrayLengthControl<T>(data: T[]) {
+  if (data.length > MAX_LENGTH) {
+    return data.slice(MAX_LENGTH - data.length)
+  }
+  return data
+}
