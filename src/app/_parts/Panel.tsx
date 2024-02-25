a662116436c279c62302172122d76d54418eef18
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2024-02-26 01:14:49 +0900

    パネルのデザイン微調整

--- src/app/_parts/Panel.tsx
+++ src/app/_parts/Panel.tsx
@@ -7,7 +7,9 @@ export const Panel = ({
 }) => {
   return (
     <section>
-      <h2 className="h-[2rem]">{name}</h2>
+      <h2 className="h-[2rem] p-1 text-center bg-slate-800">
+        {name}
+      </h2>
       <div className="h-[calc(100vh-2rem)] overflow-y-scroll">
         {children}
       </div>
