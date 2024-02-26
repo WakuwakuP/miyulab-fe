039458fe244629d2183684b27f48e2b111687a1e
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2024-02-27 04:24:06 +0900

    eslintのエラー修正

--- src/app/_components/MainPanel.tsx
+++ src/app/_components/MainPanel.tsx
@@ -44,7 +44,8 @@ export const MainPanel = () => {
     resetForm()
   }
 
-  const mediaPost = () => {}
+  // TODO: media post
+  // const mediaPost = () => {}
 
   useEffect(() => {
     if (token == null) return
