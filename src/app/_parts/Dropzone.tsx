81065d11aa62ec78a1dd6d6e64cf2b95129a2b5f
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2024-02-29 19:05:51 +0900

    クリップボードから画像の貼り付けができるように更新

--- src/app/_parts/Dropzone.tsx
+++ src/app/_parts/Dropzone.tsx
@@ -13,6 +13,7 @@ import React, {
 import imageCompression from 'browser-image-compression'
 import { Entity } from 'megalodon'
 import { useDropzone } from 'react-dropzone'
+import { CgSpinner } from 'react-icons/cg'
 
 import { GetClient } from 'util/GetClient'
 import { TokenContext } from 'util/provider/AppProvider'
@@ -24,12 +25,16 @@ export const Dropzone = ({
   children,
   attachments,
   setAttachments,
+  uploading,
+  setUploading,
 }: {
   children?: ReactNode
   attachments: Entity.Attachment[]
   setAttachments: Dispatch<
     SetStateAction<Entity.Attachment[]>
   >
+  uploading: number
+  setUploading: Dispatch<SetStateAction<number>>
 }) => {
   const token = useContext(TokenContext)
   const instance = useContext(InstanceContext)
@@ -42,13 +47,19 @@ export const Dropzone = ({
       const uploadMedia = (file: File) => {
         if (token == null) return
         const client = GetClient(token?.access_token)
-        client.uploadMedia(file).then((res) => {
-          const Attachment = res.data as Entity.Attachment
-          setAttachments((prev) => [...prev, Attachment])
-        })
+        client
+          .uploadMedia(file)
+          .then((res) => {
+            const Attachment = res.data as Entity.Attachment
+            setAttachments((prev) => [...prev, Attachment])
+          })
+          .finally(() => {
+            setUploading((prev) => prev - 1)
+          })
       }
 
       acceptedFiles.forEach((file) => {
+        setUploading((prev) => prev + 1)
         if (file.type.startsWith('image/')) {
           imageCompression(file, {
             maxSizeMB: update_limit,
@@ -62,7 +73,7 @@ export const Dropzone = ({
         }
       })
     },
-    [setAttachments, token, update_limit]
+    [setAttachments, setUploading, token, update_limit]
   )
 
   const {
@@ -99,8 +110,9 @@ export const Dropzone = ({
         <input {...getInputProps()} />
         {children}
       </div>
-      {attachments.length > 0 && (
-        <div>
+      {uploading > 0 && <div></div>}
+      {(attachments.length > 0 || uploading > 0) && (
+        <div className="flex flex-wrap">
           {attachments.map((file, index) => {
             switch (attachments.length) {
               case 1:
@@ -111,24 +123,30 @@ export const Dropzone = ({
                     className="w-full"
                   />
                 )
-              case 2:
-                return (
-                  <Media
-                    key={index}
-                    media={file}
-                    className="w-1/2"
-                  />
-                )
               default:
                 return (
                   <Media
                     key={index}
                     media={file}
-                    className="w-1/3"
+                    className="h-32 w-1/2"
                   />
                 )
             }
           })}
+          {(() => {
+            const list = []
+            for (let i = 0; i < uploading; i++) {
+              list.push(
+                <div className="flex h-32 w-1/2 items-center justify-center border bg-gray-600">
+                  <CgSpinner
+                    size={32}
+                    className="animate-spin"
+                  />
+                </div>
+              )
+            }
+            return list
+          })()}
         </div>
       )}
     </div>
