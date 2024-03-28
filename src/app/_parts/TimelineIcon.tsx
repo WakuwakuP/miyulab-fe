ab8d2737e69f86ccb43c5e1cceed55d432b2e94b
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2024-03-28 22:18:43 +0900

    マウスホイールで下にスクロールした場合にのみ追従を解除するように変更

---
+++ src/app/_parts/TimelineIcon.tsx
@@ -0,0 +1,9 @@
+import { FaStream } from 'react-icons/fa'
+
+export const TimelineStreamIcon = () => {
+  return (
+    <div className="absolute right-0 top-0 h-6 w-6 -translate-y-full">
+      <FaStream className="animate-pulse" />
+    </div>
+  )
+}
